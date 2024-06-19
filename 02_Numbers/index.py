a13fd6d12f1e07591bda29b78a2544645ab20270
TechiAkki963 <paraba777@gmail.com>
2024-06-20 01:10:04 +0530

    Numbers and Floats

---
+++ 02_Numbers/index.py
@@ -0,0 +1,67 @@
+num1=3
+print(type(num1))
+# <class 'int'>
+
+num2 = 3.14
+print(type(num2))
+# <class 'float'>
+
+# Arithmatic Operator
+# Addition : 3 + 2
+# Subtraction: 3 - 3
+# Multiplication: 3 * 3
+# Division: 3 / 3
+# Floor Division: 3 // 2
+# Exponent: 3 ** 2
+# Modules: 3 % 2
+
+d= 3/2
+print(d)
+# 1.5
+
+Fd = 3 // 2
+print(Fd)
+# 1
+
+expo = 3 ** 2
+print(expo)
+# 9
+
+mod = 3 % 2
+print(mod)
+# 1
+
+
+x = 1
+x += 1
+print(x)
+
+print(abs(-5))
+
+y = 3.76
+print(round(y , 1))
+
+
+
+# Comparison Variables
+
+a = 1
+b = 3
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+
+
+# Casting
+
+c = '100'
+d = '200'
+print(c+d)
+# 100200
+# Casting c and d
+c = int(c)
+d = int(d)
+print(c+d)
+# 300
